a9e4767ac5a586a755961fda9d55125a402143d6
F1hone <1340411740@qq.com>
2022-09-19 03:24:44 +0800

    f1sh80

--- keyboards/gh60/satan/config.h
+++ keyboards/gh60/satan/config.h
@@ -51,14 +51,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define BACKLIGHT_LEVELS 4
 
+#define BACKLIGHT_BREATHING
+
 /* Underglow configuration
  */
-#define RGB_DI_PIN E2
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 8
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
+// #define RGB_DI_PIN E2
+// #define RGBLIGHT_ANIMATIONS
+// #define RGBLED_NUM 8
+// #define RGBLIGHT_HUE_STEP 10
+// #define RGBLIGHT_SAT_STEP 17
+// #define RGBLIGHT_VAL_STEP 17
 
 /*
  * Feature disable options
