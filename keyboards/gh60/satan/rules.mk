0b0047179a1bf024378b192002be670ce3501fe3
F1hone <1340411740@qq.com>
2022-09-19 03:24:44 +0800

    f1sh80

--- keyboards/gh60/satan/rules.mk
+++ keyboards/gh60/satan/rules.mk
@@ -8,12 +8,12 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no    # Mouse keys
+MOUSEKEY_ENABLE = yes    # Mouse keys
 EXTRAKEY_ENABLE = yes   # Audio control and System control
 CONSOLE_ENABLE = yes    # Console for debug
 COMMAND_ENABLE = yes    # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-RGBLIGHT_ENABLE = yes   # Enable keyboard underlight functionality
+RGBLIGHT_ENABLE = no   # Enable keyboard underlight functionality
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
 AUDIO_ENABLE = no
 
