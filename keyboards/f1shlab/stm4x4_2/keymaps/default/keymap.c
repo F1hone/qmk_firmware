3a80a6d7ec5845abd4c26d0d824b89dab8d9ee0a
F1hone <1340411740@qq.com>
2023-11-10 19:45:41 +0800

    2023/11/10

--- keyboards/f1shlab/stm4x4_2/keymaps/default/keymap.c
+++ keyboards/f1shlab/stm4x4_2/keymaps/default/keymap.c
@@ -15,11 +15,19 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#pragma once
+#include QMK_KEYBOARD_H
 
-#ifdef HAPTIC_ENABLE
-#define SOLENOID_PIN A1
-#endif
-
-#define BUZZER_ENABLE
-#define BUZZER_PIN A0
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_ortho_4x4(
+        KC_P7,    KC_P8,  KC_P9,   KC_A,
+        KC_P4,    KC_P5,  KC_P6,   KC_B,
+        KC_P1,    KC_P2,  KC_P3,   KC_C,
+        KC_PAST,  KC_P0,  KC_PDOT, KC_D
+    ),
+    [1] = LAYOUT_ortho_4x4(
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        _______, _______, _______, _______
+    )
+};
