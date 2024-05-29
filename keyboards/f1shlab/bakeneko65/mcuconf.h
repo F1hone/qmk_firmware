f9046b0c8e370724b55751771e698e95cc2765bb
F1hone <1340411740@qq.com>
2024-05-29 16:47:24 +0800

    5/29/2

--- keyboards/f1shlab/bakeneko65/mcuconf.h
+++ keyboards/f1shlab/bakeneko65/mcuconf.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 F1shlab
+Copyright 2024 F1shlab
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
